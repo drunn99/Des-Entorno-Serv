77983a18ead2c8c8710f16e6567dc65b9568595d
drunn <drunn.99@gmail.com>
2023-12-03 13:53:30 +0100

    3-12.2 PC

--- SesionesPHP/simonDice/index.php
+++ SesionesPHP/simonDice/index.php
@@ -11,10 +11,25 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
     </head>
     <body>
         <?php
+        include "./simon.php";
+        session_start();
+        $level = isset($_SESSION["level"]) ? $_SESSION["level"] : 1;
+        $play = isset($_SESSION["play"]) ? $_SESSION["play"] == "true" : false;
 
+        if (!$play) {
+            $colorSet = createSet($level);
+        } else {
+            $colorSet = isset($_SESSION["colorSet"]) ? $_SESSION["colorSet"] : array();
+            
+        }
         ?>
         <form action=" <?php echo $_SERVER["PHP_SELF"]; ?>" method="POST">
-            <div id="matriz">
+            <div class="showSet <?php echo $play ? "nonVisible" : "visible" ?>">
+                <?php printLevel($colorSet); ?>
+                <button name="play" value="true">Start</button>
+            </div>
+
+            <div class="matrix <?php echo $play ? "visible" : "nonVisible"; ?>">
                 <button name="color" value="red"><div class="square red"></div></button>
                 <button name="color" value="yellow"><div class="square yellow"></div></button>
                 <button name="color" value="green"><div class="square green"></div></button>
@@ -22,9 +37,16 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
             </div>
         </form>
         <?php
-        var_dump($_SESSION);
-        echo "<br>";
+        
+        $_SESSION["play"] = isset($_POST["play"]) ? $_POST["play"] : false;
+        $_SESSION["colorSet"] = $colorSet;
+        $_SESSION[$level] = $level;
+        echo "POST ";
         var_dump($_POST);
+        echo "<br>";
+        echo "SES ";
+        var_dump($_SESSION);
+        echo "<br>PLAY? " . $play;
         ?>
     </body>
 </html>
--- SesionesPHP/simonDice/simon.php
+++ SesionesPHP/simonDice/simon.php
@@ -1,12 +1,25 @@
 <?php
 
-    function crearSet($level){
+    function createSet($level){
         $colors = ["red","yellow","green","blue"];
         $set = array();
-        for($i = 0; $i < $level; $i++){
+        for($i = 0; $i < ($level+2); $i++){
             $set[$i] = $colors[rand(0,3)];
         }
         return $set;
+    }
+    
+    function printLevel($set){
+        echo "<h1>Nivel: " . (count($set)-2) . " </h1>";
+        echo "<div class=\"set\">";
+        foreach ($set as $value) {
+            echo createDiv($value);
+        }
+        echo "</div>";
+    }
+    
+    function createDiv($color) : string {
+        return "<div class=\"square $color\"></div>";
     }
 /* 
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
