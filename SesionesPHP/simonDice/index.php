a9e63b610fb1ac23396b0e5ba09fa41b3642bae5
Daniel Núñez <drunn.99@gmail.com>
2023-12-04 19:18:41 +0100

    4-12.1 LP

--- SesionesPHP/simonDice/index.php
+++ SesionesPHP/simonDice/index.php
@@ -14,29 +14,26 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
         include "./simon.php";
         session_start();
         $level = isset($_SESSION["level"]) ? $_SESSION["level"] : 1;
-        $play = isset($_POST["play"]) ? boolval($_POST["play"]) : false;
+        $hitPlay = isset($_POST["play"]) ? boolval(intval($_POST["play"])) : false;
+        $play = isset($_SESSION["play"]) ? boolval($_SESSION["play"]) : false;
         $indexOfSet = isset($_SESSION["indexOfSet"]) ? $_SESSION["indexOfSet"] : 0;
 
-        if (!$play) {
+        if (!$hitPlay && !$play) {
             $colorSet = createSet($level);
         } else {
             $colorSet = isset($_SESSION["colorSet"]) ? $_SESSION["colorSet"] : array();
         }
 
         if (isset($_POST["color"])) {
-            $correct = $_POST["color"] == $colorSet[$indexOfSet];
-            $play = $correct ? true : false;
-            $indexOfSet = $correct ? $indexOfSet + 1 : 0;
-        }
-
-        if (isset($_POST["color"])) {
-            $correct = $_POST["color"] == $colorSet[$indexOfSet];
+            $correct = $_POST["color"] === $colorSet[$indexOfSet];
             if ($correct) {
+                $play = true;
                 $indexOfSet++;
                 if ($indexOfSet >= count($colorSet)) {
                     $level++;
                     $indexOfSet = 0;
                     $colorSet = createSet($level);
+                    $play = false;
                 }
             } else {
                 $play = false;
@@ -50,12 +47,12 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
         $_SESSION["indexOfSet"] = $indexOfSet;
         ?>
         <form action=" <?php echo $_SERVER["PHP_SELF"]; ?>" method="POST">
-            <div class="showSet <?php echo $play ? "nonVisible" : "visible" ?>">
+            <div class="showSet <?php echo $hitPlay ? "nonVisible" : "visible" ?>">
                 <?php printLevel($colorSet); ?>
-                <button name="play" value="true">Start</button>
+                <button name="play" value="1">Start</button>
             </div>
 
-            <div class="matrix <?php echo $play ? "visible" : "nonVisible"; ?>">
+            <div class="matrix <?php echo $hitPlay ? "visible" : "nonVisible"; ?>">
                 <button name="color" value="red"><div class="square red"></div></button>
                 <button name="color" value="yellow"><div class="square yellow"></div></button>
                 <button name="color" value="green"><div class="square green"></div></button>
@@ -68,6 +65,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
         echo "<br>";
         echo "SES ";
         var_dump($_SESSION);
+        echo "<br>OTHER: $play, $hitPlay";
         ?>
     </body>
 </html>
