24251f1a22398bf24e171d65c9be4722350105dc
Daniel Núñez <drunn.99@gmail.com>
2023-12-11 10:20:12 +0100

    11-12.1 LP

--- SesionesPHP/simonDice/index.php
+++ SesionesPHP/simonDice/index.php
@@ -14,58 +14,82 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
         include "./simon.php";
         session_start();
         $level = isset($_SESSION["level"]) ? $_SESSION["level"] : 1;
-        $hitPlay = isset($_POST["play"]) ? boolval(intval($_POST["play"])) : false;
-        $play = isset($_SESSION["play"]) ? boolval($_SESSION["play"]) : false;
+        $play = isset($_POST["play"]) ? $_POST["play"] == "true" : (isset($_SESSION["play"]) ? $_SESSION["play"] : false);
         $indexOfSet = isset($_SESSION["indexOfSet"]) ? $_SESSION["indexOfSet"] : 0;
-
-        if (!$hitPlay && !$play) {
+        $showSet = isset($_SESSION["showSet"]) ? $_SESSION["showSet"] : array();
+        $wrongMessage = false;
+        $correctDone = false;
+        //Comprobar que no se esté jugando para crear un nuevo set o mantener el actual
+        if (!$play) {
             $colorSet = createSet($level);
         } else {
             $colorSet = isset($_SESSION["colorSet"]) ? $_SESSION["colorSet"] : array();
         }
-
+        //Si el usuario ha pulsado un color comprueba si es correcto o no.
         if (isset($_POST["color"])) {
-            $correct = $_POST["color"] === $colorSet[$indexOfSet];
+            $correct = $indexOfSet < count($colorSet) ? $_POST["color"] === $colorSet[$indexOfSet] : false;
             if ($correct) {
+                array_push($showSet, $colorSet[$indexOfSet]);
                 $play = true;
                 $indexOfSet++;
-                if ($indexOfSet >= count($colorSet)) {
-                    $level++;
-                    $indexOfSet = 0;
-                    $colorSet = createSet($level);
-                    $play = false;
-                }
             } else {
-                $play = false;
-                $indexOfSet = 0;
+                $wrongMessage = true;
+                array_push($showSet, $_POST["color"]);
             }
         }
-        
+        //Si se ha completado el set de colores pasa al siguiente nivel
+        if ($indexOfSet >= count($colorSet)) {
+            $correctDone = true;
+            if (isset($_POST["nextLevel"])) {
+                nextLevel($play, $level, $indexOfSet, $showSet, $colorSet);
+                $correctDone = false;
+            }
+        }
+        //Si se ha pulsado el botón de restart renicia el nivel,
+        if (isset($_POST["wrongMessage"])) {
+            resetLevel($play, $level, $indexOfSet, $showSet, $colorSet);
+            $wrongMessage = false;
+        }
+
         $_SESSION["level"] = $level;
         $_SESSION["play"] = $play;
         $_SESSION["colorSet"] = $colorSet;
         $_SESSION["indexOfSet"] = $indexOfSet;
+        $_SESSION["showSet"] = $showSet;
         ?>
         <form action=" <?php echo $_SERVER["PHP_SELF"]; ?>" method="POST">
-            <div class="showSet <?php echo $hitPlay ? "nonVisible" : "visible" ?>">
+            <div class=" <?php echo $play ? "nonVisible" : "visible"?>">
                 <?php printLevel($colorSet); ?>
-                <button name="play" value="1">Start</button>
+                <button name="play" value="true">Start</button>
             </div>
 
-            <div class="matrix <?php echo $hitPlay ? "visible" : "nonVisible"; ?>">
-                <button name="color" value="red"><div class="square red"></div></button>
-                <button name="color" value="yellow"><div class="square yellow"></div></button>
-                <button name="color" value="green"><div class="square green"></div></button>
-                <button name="color" value="blue"><div class="square blue"></div></button>
+            <div class="matrix <?php echo $play ? "" : "nonVisible"; ?>">
+                <button name="color" value="red" <?php echo $wrongMessage || $correctDone ?  "disabled" : "" ?>><div class="square red simon"></div></button>
+                <button name="color" value="yellow" <?php echo $wrongMessage || $correctDone ?  "disabled" : "" ?>><div class="square yellow simon"></div></button>
+                <button name="color" value="green" <?php echo $wrongMessage || $correctDone ?  "disabled" : "" ?>><div class="square green simon"></div></button>
+                <button name="color" value="blue" <?php echo $wrongMessage || $correctDone ?  "disabled" : "" ?>><div class="square blue simon"></div></button>
             </div>
+
+            <div class="set">
+                <?php
+                foreach ($showSet as $value) {
+                    echo createDiv($value);
+                }
+                ?>
+            </div>
+            
+            <div class="<?php echo $correctDone ? "" : "nonVisible" ?>">
+                <h2>!Correcto!</h2>
+                <button name="nextLevel" value="true" >Siguiente</button>
+            </div>
+            
+            <div class ="<?php echo $wrongMessage ? "" : "nonVisible" ?>">
+                <h2>!Has fallado!</h2>
+                <button name="wrongMessage" value="true" >Reiniciar</button>
+            </div>
+            
         </form>
         <?php
-        echo "POST ";
-        var_dump($_POST);
-        echo "<br>";
-        echo "SES ";
-        var_dump($_SESSION);
-        echo "<br>OTHER: $play, $hitPlay";
         ?>
     </body>
 </html>
--- SesionesPHP/simonDice/simon.php
+++ SesionesPHP/simonDice/simon.php
@@ -1,26 +1,48 @@
 <?php
 
-    function createSet($level){
-        $colors = ["red","yellow","green","blue"];
-        $set = array();
-        for($i = 0; $i < ($level+2); $i++){
-            $set[$i] = $colors[rand(0,3)];
-        }
-        return $set;
+//Crea un array con colores aleatorios
+function createSet($level) {
+    $colors = ["red", "yellow", "green", "blue"];
+    $set = array();
+    for ($i = 0; $i < ($level + 2); $i++) {
+        $set[$i] = $colors[rand(0, 3)];
     }
-    
-    function printLevel($set){
-        echo "<h1>Nivel: " . (count($set)-2) . " </h1>";
-        echo "<div class=\"set\">";
-        foreach ($set as $value) {
-            echo createDiv($value);
-        }
-        echo "</div>";
+    return $set;
+}
+
+//Imprime el nivel
+function printLevel($set) {
+    echo "<h1>Nivel: " . (count($set) - 2) . " </h1>";
+    echo "<div class=\"showSet\">";
+    foreach ($set as $value) {
+        echo createDiv($value);
     }
-    
-    function createDiv($color) : string {
-        return "<div class=\"square $color\"></div>";
-    }
+    echo "</div>";
+}
+
+//Genera un cuadrado de 100px * 100px con el color correspondiente
+function createDiv($color): string {
+    return "<div class=\"square $color\"></div>";
+}
+
+//Reinicia el nivel a 1 y el resto de parámetros del juego.
+function resetLevel(&$play, &$level, &$indexOfSet, &$showSet, &$colorSet) {
+    $level = 1;
+    $indexOfSet = 0;
+    $colorSet = createSet($level);
+    $play = false;
+    $showSet = array();
+}
+
+//Sube de nivel y reinicia el resto de parámetros del juego.
+function nextLevel(&$play, &$level, &$indexOfSet, &$showSet, &$colorSet) {
+    $level++;
+    $indexOfSet = 0;
+    $colorSet = createSet($level);
+    $play = false;
+    $showSet = array();
+}
+
 /* 
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
